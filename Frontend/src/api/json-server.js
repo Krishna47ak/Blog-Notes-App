489cffa5dc1e56727fc67e6e7806634edba3ce95
Krishna47ak <anandakrishna720@gmail.com>
2023-07-25 22:38:24 +0530

    Changed folder structure and added backend

---
+++ Frontend/src/api/json-server.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL:'https://2ac8-2402-3a80-1a91-c778-9565-58c9-fb57-db4b.ngrok.io'
+})
--- src/api/json-server.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL:'https://19d3-157-50-17-172.in.ngrok.io'
-})
